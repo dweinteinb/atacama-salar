a6e30da9d1c8e2a54ef07e444a7305ef6244270b
David Weinstein <67246404+dweinteinb@users.noreply.github.com>
2021-08-24 20:39:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st. title('Search in Pubmed')
-#st. subheader('Find the papers between')
+st. subheader('Enter a microbiology keyword')
 #st. text('select keyword')
 #st. text('select abstract')
+
+user_input = st.text_input('Keyword: ', 'Salar') #  single line of text. 2nd is default
