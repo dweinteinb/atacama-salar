71775e785858b410f8c548d67c7d03e1d379014d
David Weinstein <67246404+dweinteinb@users.noreply.github.com>
2021-08-24 20:48:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,15 @@ st. subheader('Enter a microbiology keyword')
 #st. text('select keyword')
 #st. text('select abstract')
 
+# Input box
 user_input = st.text_input('Keyword: ', 'Salar') #  single line of text. 2nd is default
+
+# hide footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
