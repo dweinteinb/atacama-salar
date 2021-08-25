dc24ab180779976dd3c8f983530580efbe7d0bde
David Weinstein <67246404+dweinteinb@users.noreply.github.com>
2021-08-24 22:49:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
-st. title('Search in Pubmed')
-st. subheader('Enter a microbiology keyword')
-#st. text('select keyword')
-#st. text('select abstract')
+header = st.beta_container()
+dataset = st.beta_container()
+
+with header :
+    st. title('Search in Pubmed')
+    #st. subheader('Enter a microbiology keyword')
+    #st. text('Enter a microbiology keyword')
+    #st. text('select abstract')
 
 # Input box
 user_input = st.text_input('Keyword: ', 'Salar') #  single line of text. 2nd is default
+
+
+
+
 
 # hide footer
 hide_streamlit_style = """
